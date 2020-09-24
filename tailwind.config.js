7c69087807f65d9a516c67f28016c9645a5d4ff0
Xavier La Rosa <38988734+XavierLaRosa@users.noreply.github.com>
2020-09-24 09:27:53 -0400

    Added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.html', './src/**/*.ts'],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
